815bd3c9f4c726774db194ee1b7e7c174da7a815
Tiago Abade Dias <tdias@aktienow.com>
2019-12-11 11:15:00 -0300

    Ajutes

--- src/app.js
+++ src/app.js
@@ -6,12 +6,10 @@ const router = express.Router();
 
 //Rotas
 const index = require('./routes/index');
-const personRoute = require('./routes/personRoute');
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 app.use('/', index);
-app.use('/person', personRoute);
 
 module.exports = app;
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,10 +3,19 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function (req, res, next) {
-    res.status(200).send({
-        title: "Node Express API",
-        version: "0.0.1"
-    });
+    console.log(" - - - - ");
+
+    console.log(req);
+ 
+    console.log(" - - - - ");
+
+    console.log(" - - - - ");
+
+    console.log(req.body);
+ 
+    console.log(" - - - - ");
+
+    res.status(200).send(req.body);
 });
 
 module.exports = router;
